4a533a03cdeb753c2039f691b22046755de001d1
ilinzweilin <alina@centrifuge.io>
2020-06-09 20:04:37 +0200

    remove config log

---
+++ index.js
@@ -0,0 +1,9 @@
+const kovanStaging = require('./kovan-staging.json');
+const mainnetProduction = require('./mainnet-production.json');
+
+const poolConfigs = {
+    kovanStaging,
+    mainnetProduction
+}
+
+module.exports = poolConfigs
