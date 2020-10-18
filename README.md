136accc9e9aadd011c73e6d3128c1bb508847bbf
Lucas Vogelsang <l@lucasvo.com>
2020-10-18 13:55:40 +0000

    registry CLI

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# tinlake-config
+# tinlake-pool-metadata
+This repo stores pool metadata of active tinlake pools along with an on chain registry.
+
+## Setup
+1) Install dependencies with `npm install`
+2) Get an API key on pinata and set the env variables `PINATA_API_KEY` and `PINATA_SECRET_KEY`
+3) Set the `dapp` typical env variables (`ETH_RCP_URL`,
+
+## PoolRegistry
+### Usage
+The registry keeps track of a list of addresses along with their short name and pool metadata IPFS hash.
+
+### `pool` CLI
+### Deployment
+To create a new pool registry, deploy it with:
+```
+    dapp create --verify "src/registry.sol:PoolRegistry"
+```
--- src/registry.sol
+++ src/registry.sol
@@ -1,36 +1,52 @@
-pragma solidity ^0.6.7;
+pragma solidity ^0.5.15;
 pragma experimental ABIEncoderV2;
 
-struct Pool {
-    address pool;
-    bool    live;
-    string  name;
-    string  data;
-}
+import "tinlake-auth/auth.sol";
 
-contract Registry {
+contract PoolRegistry is Auth {
+    struct Pool {
+        address pool;
+        bool    live;
+        string  name;
+        string  data;
+    }
+
+    event PoolCreated(address pool, bool live, string name, string data);
+    event PoolUpdated(address pool, bool live, string name, string data);
+
+
+
     Pool[] public pools;
-    mapping (address => uint) lookup;
+    mapping (address => uint) public lookup;
 
 
     constructor() public {
+        wards[msg.sender] = 1;
         pools.push(Pool(address(this), false, "registry", ""));
     }
 
-    function file(address pool, bool live, string memory name, string memory data) public {
+    function file(address pool, bool live, string memory name, string memory data) public auth {
         Pool memory p = Pool(pool, live, name, data);
+        if (address(this) == pool) {
+            pools[0] = p;
+            return;
+        }
+
         uint index = lookup[pool];
         if (index > 0) {
             pools[index] = p;
+            emit PoolUpdated(pool, live, name, data);
         } else {
             pools.push(p);
             lookup[pool] = pools.length -1;
+            emit PoolCreated(pool, live, name, data);
         }
     }
 
-    function find(address pool) public view returns (Pool memory) {
+    function find(address pool) public view returns (bool live, string memory name, string memory data) {
         require(lookup[pool]>0, "pool-not-found");
-        return pools[lookup[pool]];
+        Pool memory p = pools[lookup[pool]];
+        return (p.live, p.name, p.data);
     }
 
 }
