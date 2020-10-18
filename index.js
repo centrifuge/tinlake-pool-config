31eb5305324b7a0f3846f27d8bddf13c387cc8d3
Lucas Vogelsang <l@lucasvo.com>
2020-10-18 09:57:32 +0000

    Dynamically combining metadata from folder

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const kovanStaging = require('./kovan-staging.json');
+const fs = require('fs')
+
+function loadMetadata(folder) {
+  let pools = [];
+  fs.readdirSync(folder).forEach(function(file) {
+    if (file.match(/\.json$/) !== null) {
+      var name = file.replace('.js', '')
+      pools.push(require(folder + file));
+    }
+  });
+  return pools;
+}
+
+const kovanPools = __dirname + '/pools/kovan/metadata/'
 const mainnetProduction = require('./mainnet-production.json');
 
 const poolConfigs = {
-    kovanStaging,
-    mainnetProduction
+  kovanStaging: loadMetadata(kovanPools),
+  mainnetProduction
 }
-
+console.log(poolConfigs)
 module.exports = poolConfigs
